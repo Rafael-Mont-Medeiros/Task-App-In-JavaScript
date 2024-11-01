ba979d40265e4e8c982a01bcc9866bbd0f5a8cde
Rafael-Mont-Medeiros <rafak1212@gmail.com>
2024-11-01 08:58:36 -0300

    app de tarefas, com script de marcar, desmarcar e deletar a tarefa...mais funções em breve.

--- assets/script.js
+++ assets/script.js
@@ -5,20 +5,33 @@ function addTask(){
     if(inputBox.value === ''){
         alert('Adicione uma tarefa! ( Add a task! )');
     }else {
-        let li = document.createElement('li');
-        li.innerHTML = inputBox.value;
-        listContainer.appendChild(li);
+        let list = document.createElement('li');
+        list.innerHTML = inputBox.value;
+        listContainer.appendChild(list);
         let span = document.createElement('span');
         span.innerHTML = '\u00d7'
-        li.appendChild(span)
+        list.appendChild(span)
     }
-    inputBox.value = ''
+    inputBox.value = '';
+    dataSave()
 }
 
 listContainer.addEventListener('click', (e)=>{
     if(e.target.tagName === 'LI'){
         e.target.classList.toggle("checked");
+        dataSave();
     }else if(e.target.tagName === 'SPAN'){
         e.target.parentElement.remove();
+        dataSave();
     }
-},false)
+},false);
+
+function dataSave(){
+    localStorage.setItem('data', listContainer.innerHTML);
+};
+
+function taskShow(){
+    listContainer.innerHTML = localStorage.getItem('data');
+};
+
+taskShow()
